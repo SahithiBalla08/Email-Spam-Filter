355cd734b9dc4aca7bbbf5583a00db0b07172a1b
Sahithi Balla <SahithiBalla2004@gmail.com>
2025-09-20 12:39:20 +0530

    Upload Email Spam Filter project

---
+++ app.py (2).py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import streamlit as st
+
+
+# In[2]:
+
+
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+ps = PorterStemmer()
+
+
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+#tfidf = pickle.load(open('vectorizer (1).pkl','rb'))
+tfidf = pickle.load(open('/Users/sahit/OneDrive/New folder/OneDrive/Desktop/email-spam filter/vectorizer (1).pkl','rb'))
+model = pickle.load(open('/Users/sahit/OneDrive/New folder/OneDrive/Desktop/email-spam filter/model.pkl','rb'))
+
+st.title("EMAIL-SPAM FILTER")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict((vector_input)[0])
+    # 4. Display
+    if result == 1:
+        st.header("SPAM")
+    else:
+        st.header("NOT SPAM")
+
+
+
+
+
+
+
